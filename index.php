617b733c97c2fc3e9625717eb4c4c6210128286b
user <daradosu@gmail.com>
2015-09-29 08:46:51 +0100

    Get rid of Selector

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
 include 'vendor/autoload.php';
-use Connection;
-$conn = new Connection;
+
+use Dara\Connection;
+
+$driver = 'mysql';
+$host   = 'localhost';
+$db     = 'todosql';
+$user   = 'root';
+$pass   = '';
+
+
+$conn = new Connection($driver, $host, $db, $user, $pass);
+
+$conn->selectDb($db);
+
+$table = 'todos' ;
+$col1  = 'title';
+$col2  = 'description';
+
+$conn->getToDos($table, $col1, $col2);
